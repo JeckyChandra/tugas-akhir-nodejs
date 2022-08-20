d83fc3ef545c13db14a8323e20cac81f748c4c9c
JeckyChandra <jackfour298@gmail.com>
2022-08-20 16:45:09 +0700

    Websocket realtime chat

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dbConfig = require("./config/database.config.js");
 const mongoose = require("mongoose");
-var cors = require("cors");
+const cors = require("cors");
 const userRoutes = require("./app/routes/user.routes");
+const { Server } = require('socket.io')
+const { createServer } = require("http");
+const fs = require('fs/promises');
+
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
@@ -40,6 +44,49 @@ app.get("/", (req, res) => {
 
 require("./app/routes/note.routes.js")(app);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is listening on port 3000");
+const httpServer = createServer(app);
+
+const server = new Server(httpServer, {
+    cors:{
+        origin:'http://localhost:3000'
+    }
+})
+
+server.on("connection", async(socket)=>{
+    const getChat = await fs.readFile('./chat.json', 'utf-8');
+
+    const chatToJson = JSON.parse(getChat);
+
+    socket.emit('chat', {data: chatToJson});
+
+
+    socket.on('message', async (...data) => {
+		console.log(data[0].data, '<<< data');
+		const getData = data[0].data;
+
+        console.log(chatToJson);
+
+        console.log(typeof chatToJson);
+
+
+		chatToJson[getData.sender].push({
+			sender: getData.sender,
+			message: getData.message
+		})
+
+		chatToJson[getData.to].push({
+			sender: getData.sender,
+			message: getData.message
+		})
+
+		await fs.writeFile('./chat.json', JSON.stringify(chatToJson), 'utf-8')
+    })
+})
+
+httpServer.listen(3000, () => {
+	console.log(`App listen to port 3000`)
 });
+
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
